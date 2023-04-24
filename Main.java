2e267edaf706f444db2cbfd2e19809cf31c25b1e
Mazin Habib <mazin.habib@live.com>
2023-04-24 09:53:37 -0400

    added edit/add/delete features and writing to file

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,13 +12,17 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
-
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 public class Main extends Application {
 
@@ -30,6 +36,24 @@ public class Main extends Application {
 
     static String[] values;
 
+    // Create a private TableView object called contactTable
+    private TableView contactTable = new TableView();
+
+    // Create a private TextField object called firstName
+    private TextField firstName = new TextField();
+
+    // Create a private TextField object called lastName
+    private TextField lastName = new TextField();
+
+    // Create a private TextField object called companyName
+    private TextField companyName = new TextField();
+
+    // Create a private TextField object called email
+    private TextField email = new TextField();
+
+    // Create a private TextField object called number
+    private TextField number = new TextField();
+
     public static void main(String[] banana) {
         launch(banana);
     }
@@ -41,30 +65,36 @@ public class Main extends Application {
         Pane menuPane = new Pane();
 
         // Creating the JavaFX Scene and defining the pane and dimensions of the window
-        Scene menuScene = new Scene(menuPane, 600, 600);
+        Scene menuScene = new Scene(menuPane, 800, 600);
 
         // Creating a TableView Object for making a table to display on screen
-        TableView contactTable = new TableView();
+        // TableView contactTable = new TableView();
+        contactTable.setEditable(true);
 
         // Creating the "First Name" column
         TableColumn<Contact, String> firstNameColumn = new TableColumn<Contact, String>("First Name");
         firstNameColumn.setCellValueFactory(new PropertyValueFactory<Contact, String>("fName"));
+        firstNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
         // Creating the "Last Name" column
         TableColumn<Contact, String> lastNameColumn = new TableColumn<Contact, String>("Last Name");
         lastNameColumn.setCellValueFactory(new PropertyValueFactory<Contact, String>("lName"));
+        lastNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
         // Creating the "Company" column
         TableColumn<Contact, String> companyColumn = new TableColumn<Contact, String>("Company");
         companyColumn.setCellValueFactory(new PropertyValueFactory<Contact, String>("cmpny"));
+        companyColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
         // Creating the "Email Address" column
         TableColumn<Contact, String> emailAddressColumn = new TableColumn<Contact, String>("Email Address");
         emailAddressColumn.setCellValueFactory(new PropertyValueFactory<Contact, String>("eAddress"));
+        emailAddressColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
         // Creating the "Phone Number" column
         TableColumn<Contact, String> phoneNumberColumn = new TableColumn<Contact, String>("Phone Number");
         phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<Contact, String>("pNumber"));
+        phoneNumberColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
         // Adding all of the columns to the TableView Object created earlier called
         // "contactTable"
@@ -73,6 +103,151 @@ public class Main extends Application {
         contactTable.getColumns().add(companyColumn);
         contactTable.getColumns().add(emailAddressColumn);
         contactTable.getColumns().add(phoneNumberColumn);
+
+        // relocate the table to coordinates (165, 85)
+        contactTable.relocate(165, 85);
+
+        // set the minimum width of the table to 470
+        contactTable.setMinWidth(470);
+
+        // set a prompt text for the TextField fristName which says "First Name"
+        firstName.setPromptText("First Name");
+
+        // add the TextField firstName to menuLayout
+        menuPane.getChildren().add(firstName);
+
+        // relocate firstName to coordinates (20, 20)
+        firstName.relocate(20, 20);
+
+        // set the maximum width of firstName to 100
+        firstName.setMaxWidth(100);
+
+        // set a prompt text for the TextField lastName which says "Last Name"
+        lastName.setPromptText("Last Name");
+
+        // add the TextField lastName to menuLayout
+        menuPane.getChildren().add(lastName);
+
+        // relocate lastName (130, 20)
+        lastName.relocate(130, 20);
+
+        // set the maximum width of lastName to 100
+        lastName.setMaxWidth(100);
+
+        // set a prompt text for the TextField companyName which says "Company"
+        companyName.setPromptText("Company");
+
+        // add the TextField companyName to menuLayout
+        menuPane.getChildren().add(companyName);
+
+        // relocate companyName to (240, 20)
+        companyName.relocate(240, 20);
+
+        // set the maximum width of companyName to 120
+        companyName.setMaxWidth(120);
+
+        // set a prompt text for the TextField email which says "Email Address"
+        email.setPromptText("Email Address");
+
+        // add the TextField email to menuLayout
+        menuPane.getChildren().add(email);
+
+        // relocate email to (370, 20)
+        email.relocate(370, 20);
+
+        // set the minimum width of email to be 200
+        email.setMinWidth(200);
+
+        // set a prompt text for the TextField number which says "Phone Number"
+        number.setPromptText("Phone Number");
+
+        // add the TextField number to menuLayout
+        menuPane.getChildren().add(number);
+
+        // relocate number to (580, 20)
+        number.relocate(580, 20);
+
+        // set the maximum width of number to 100
+        number.setMaxWidth(100);
+
+        // create a Button called addButton for adding contacts
+        Button addButton = new Button("Add");
+
+        // add the button to menuPane
+        menuPane.getChildren().add(addButton);
+
+        // relocate it to (690, 20)
+        addButton.relocate(690, 20);
+
+        // set the minimum width of it to 90
+        addButton.setMinWidth(90);
+
+        // Create an event handler for when the button is pressed
+        addButton.setOnAction(event -> {
+
+            // Create a contact object with the values in the TextFields to be the values of
+            // the properties of the Contact
+            Contact person = new Contact(firstName.getText(), lastName.getText(), companyName.getText(),
+                    email.getText(), number.getText());
+
+            // Call the addContact method that adds the contact to the table
+            addContact(person);
+
+            // Call the clearFields method that clears the TextFields
+            clearFields();
+
+        });
+
+        // Create a button called delete for deleting contacts
+        Button deleteButton = new Button("Delete");
+
+        // Add it to menuPane
+        menuPane.getChildren().add(deleteButton);
+
+        // Relocate it to (690, 60)
+        deleteButton.relocate(690, 60);
+
+        // Set the minimum width to 90
+        deleteButton.setMinWidth(90);
+
+        // Create an event handler for when the button is pressed
+        deleteButton.setOnAction(event -> {
+
+            // Create an int variable called row to store the index value of the Contact
+            // being deleted
+            int row = contactTable.getSelectionModel().getSelectedIndex();
+
+            // Remove the Contact with the index row
+            contactTable.getItems().remove(row);
+
+        });
+
+        window.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent e) {
+
+                String path = "contactlist.csv";
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(path, false));
+
+                    for (Object r : contactTable.getItems()) {
+                        String firstName = ((Contact) r).fNameProperty().get();
+                        String lastName = ((Contact) r).lNameProperty().get();
+                        String company = ((Contact) r).cmpnyProperty().get();
+                        String eAddress = ((Contact) r).eAddressProperty().get();
+                        String pNumber = ((Contact) r).pNumberProperty().get();
+
+                        System.out.println(firstName + "," + lastName + "," + company + "," + eAddress + "," + pNumber);
+                        writer.write(
+                                firstName + "," + lastName + "," + company + "," + eAddress + "," + pNumber);
+                        writer.newLine();
+                    }
+                    writer.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+        });
 
         // Initializing the path of the .csv file
         String path = "contactlist.csv";
@@ -112,4 +287,34 @@ public class Main extends Application {
         // Displaying the application window
         window.show();
     }
+
+    // A private void method called addContact with the parameter of type Contact
+    // called person. Its function is to add person to the contactTable
+    private void addContact(Contact Person) {
+
+        // Add person to contactTable
+        contactTable.getItems().add(Person);
+    }
+
+    // A private void method called ClearFields that doesn't have any parameters.
+    // Its function is to clear the TextFields
+    private void clearFields() {
+
+        // Clear firstName
+        firstName.clear();
+
+        // Clear lastName
+        lastName.clear();
+
+        // Clear companyName
+        companyName.clear();
+
+        // Clear email
+        email.clear();
+
+        // Clear number
+        number.clear();
+
+    }
+
 }
